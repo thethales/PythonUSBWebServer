00135afc8ee97a99443fb27f7657c3a856561b65
the.thales <thvms.thales@gmail.com>
2020-12-05 12:22:48 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # PythonUSBWebServer
-A simple standalone http webserver based on python
+
+A simple standalone http webserver based on python, leveraging Python's built-in ```http.server``` library
+
+
+
+
+# Dev
+
+```
+pip install pyyaml
+
+```
+
+
+
+# Config YAML
+
+
+```webbrowser```
+ 
+|Key|Type|Description|Default|
+|---|----|-----------|-------|
+|port|```integer```| Specifies the web server port. Make sure to fill in a avaliable port, current version does not check for port avaliability | 8080 |
+|directory| ```string```| Specifies the directory to run server and serve the webpages| Current directory|
+
+```webbrowser```
+ 
+|Key|Type|Description|Default|
+|---|----|-----------|-------|
+|open_url_after_launch| ```boolean```| If ```true``` opens the webserver url after launch on the default OS browser| ```true```|
+|window| ```integer```| Displays the url using the default browser. If  0, the url is opened in the same browser window if possible. If the value is 1, a new browser window is opened if possible. If value is 2, a new browser page (“tab”) is opened if possible. | ```0```|
+|autoraise| ```boolean``` | If autoraise is True, the window is raised if possible (note that under many window managers this will occur regardless of the setting of this variable). |```true```|
+|url|```string``` |Specifies custom URL | _http://localhost:<server_port>_  e.g. _http://localhost:8080_|
---
+++ run.py
@@ -0,0 +1,53 @@
+import os
+import webbrowser
+import yaml
+from http.server import HTTPServer, CGIHTTPRequestHandler
+
+
+config_file         = open("config.yaml")
+config              = yaml.load(config_file, Loader=yaml.FullLoader)
+current_directory   = os.path.dirname(os.path.realpath(__file__))
+
+
+
+# Validate Config.yaml
+if config["httpserver"]["port"] is None:
+    print("Server port not configured, assuming default port: 8080")
+    config["httpserver"]["port"] = 8080
+
+if config["httpserver"]["directory"] is None:
+    print("Server Directory not configured, assuming default directory: " + current_directory)
+    config["httpserver"]["directory"] = current_directory
+
+if config["webbrowser"]["open_url_after_launch"] is None:
+    print("'Open URL After Launch' not configured, assuming default: True")
+    config["webbrowser"]["open_url_after_launch"] = True
+
+if config["webbrowser"]["window"] is None:
+    print("'Browser tab configuration' not epecified, assuming default: 1 - A New Browser Windows is opned is possible")
+    config["webbrowser"]["window"] = 1
+
+if config["webbrowser"]["autoraise"] is None:
+    print("'Browser windows autoraise' not epecified, assuming default: True")
+    config["webbrowser"]["autoraise"] = True
+
+if config["webbrowser"]["url"] is None:
+    print("'Browser URL' not epecified, assuming default: http://localhost")
+    config["webbrowser"]["url"] = 'http://localhost:' + str(config["httpserver"]["port"])
+
+
+
+
+
+os.chdir(config["httpserver"]["directory"])
+# Create server object 
+server_object = HTTPServer(server_address=('', config["httpserver"]["port"]), RequestHandlerClass=CGIHTTPRequestHandler)
+
+if config["webbrowser"]["open_url_after_launch"]:
+    # Open Web Page on Default Browser if Possible
+    webbrowser.open('http://localhost:8080', new=config["webbrowser"]["window"], autoraise=config["webbrowser"]["autoraise"])
+
+server_object.serve_forever()
+
+
+
